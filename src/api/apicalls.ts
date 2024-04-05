4a75fbe2381a14ecfefb0e1357f371e27f202f6d
ozankislali <ozankislali36@gmail.com>
2024-04-05 09:58:18 +0300

    api key eklenmesi

---
+++ src/api/apicalls.ts
@@ -0,0 +1,3 @@
+const apikey :string ='6f4c0fa2adf5b8c5bf6c6d4d0b29c5f4';
+export const nowPlayingMovies:string =`https://api.themoviedb.org/3/movie/now_playing?api_key${apikey}`;
+export const upcomimgMovies:string=`https://api.themoviedb.org/3/movie/upcoming?api_key${apikey}`
